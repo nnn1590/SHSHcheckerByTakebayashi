399ed50b30294ecca9c4f9102d1b243a6c4e03af
NNN1590 <nnn1590@nnn1590.org>
2021-12-26 01:54:13 +0900

    Update README.md and shsh.sh

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-# Issue SHSH checker
-復元可能なiOSバージョンをベータ版も含め簡単に確認できるbashスクリプトです
+# SHSH checker (fork)
+復元可能なiOSバージョンをベータ版も含め簡単に確認できるbashスクリプトです。
 
 ![bash <(curl -s https://otintin.world/shsh.sh)](Docs/test.jpg)
+
+実行にはインターネット接続が必要です。
 
 ## 依存関係
 JSONファイルを解析するためjqコマンドが必要です。
@@ -12,15 +14,16 @@ sudo apt install jq
 ```
 
 ## 仕組み
-[SHSH Host](https://shsh.host)からJSON情報を取得しiPhone9,4の発行状況を確認します。取得元の情報によっては最新ではない可能性があるため表示されたバージョンで復元できる保証はできません。
+[SHSH Host](https://shsh.host)からJSON情報を取得しSHSHの発行状況を確認します。取得元の情報によっては最新ではない可能性があるため表示されたバージョンで復元できる保証はできません。
 
 ## 実行の仕方
-`shsh.sh`を実行すると検索が開始されます
+`shsh.sh`を実行すると確認が開始されます。
+第一引数には対象となる端末を指定できます。（未指定の場合`iPhone9,1`になります。）
 ```bash
-bash -c ./shsh.sh
+./shsh.sh iPhone9,1
 ```
 
-以下のコマンドを使用すればgitからcloneすることなく実行することも可能です
+以下のコマンドを使用すればgitからcloneすることなく実行することも可能です]
 ```bash
-curl -s https://raw.githubusercontent.com/nnn1590/SHSHcheckerByTakebayashi/fork1/shsh.sh | bash -
+curl -s https://raw.githubusercontent.com/nnn1590/SHSHcheckerByTakebayashi/fork1/shsh.sh | bash -s - iPhone9,1
 ```
--- shsh.sh
+++ shsh.sh
@@ -1,10 +1,20 @@
+#!/usr/bin/env bash
 #取得可能なSHSHを検索し一覧にします
-echo "#Issue SHSH checker Ver.1.0"
-curl -s https://shsh.host >> cache.txt && tail cache.txt -n 3 | head -n 1 >> tes.txt && rm cache.txt
-sed -e 's/var signed_array =//g' tes.txt > te.txt
-rm tes.txt
-cat te.txt | jq . >> test.txt 2>/dev/null
-rm te.txt
-cat test.txt | jq -S ' ."iPad6,11"' | sed -e 's/{//g' | sed -e 's/"//g' | sed -e 's/://g' | sed -e 's/}//g' | sed -e 's/,//g' |  sed -e '1d' | sed -e '$d'
-echo 以上`cat test.txt | jq -S ' ."iPhone9,4"' | sed -e 's/{//g' | sed -e 's/"//g' | sed -e 's/://g' | sed -e 's/}//g' | sed -e 's/,//g' |  sed -e '1d' | sed -e '$d' | wc -l`個のSHSH取得が可能です。
-rm test.txt
+
+set -eu
+
+declare TMPFILE DEVICE
+TMPFILE="$(mktemp)"
+DEVICE="${1:-iPhone9,4}"
+readonly TMPFILE DEVICE
+
+trap 'rm "${TMPFILE}"' EXIT
+
+echo "# SHSH checker Ver.1.0.1 (fork)"
+echo "対象の端末: ${DEVICE}"
+curl -s https://shsh.host | grep 'var signed_array =' | sed -Ee 's/(var signed_array =|;\r?$)//g' > "${TMPFILE}"
+jq -rS ".\"${DEVICE}\""' | to_entries[] | "\t\(.key) - \(.value)"' "${TMPFILE}" || {
+	echo "エラー: 一覧の取得に失敗しました。端末「${DEVICE}」が存在しないなどの原因が考えられます。" >&2
+	exit 1
+}
+echo "以上$(jq -rS ".\"${DEVICE}\""' | length' "${TMPFILE}")個のSHSH取得が可能です。"
