53deda0d49b73dbcd05840d8b5d4976bb743a993
NNN1590 <nnn1590@nnn1590.org>
2022-01-29 22:40:30 +0900

    shsh.sh: Use FIFO instead of using temp file

--- shsh.sh
+++ shsh.sh
@@ -3,18 +3,16 @@
 
 set -eu
 
-declare TMPFILE DEVICE
-TMPFILE="$(mktemp)"
+declare DEVICE JSON
 DEVICE="${1:-iPhone9,4}"
-readonly TMPFILE DEVICE
-
-trap 'rm "${TMPFILE}"' EXIT
+readonly DEVICE
 
 echo "# SHSH checker Ver.1.0.1 (fork)"
 echo "対象の端末: ${DEVICE}"
-curl -s https://shsh.host | grep 'var signed_array =' | sed -Ee 's/(var signed_array =|;\r?$)//g' > "${TMPFILE}"
-jq -rS ".\"${DEVICE}\""' | to_entries[] | "\t\(.key) - \(.value)"' "${TMPFILE}" || {
+JSON="$(curl -s https://shsh.host | grep 'var signed_array =' | sed -Ee 's/(var signed_array =|;\r?$)//g')"
+readonly JSON
+jq -rS ".\"${DEVICE}\""' | to_entries[] | "\t\(.key) - \(.value)"' <(printf "%s" "${JSON}") || {
 	echo "エラー: 一覧の取得に失敗しました。端末「${DEVICE}」が存在しないなどの原因が考えられます。" >&2
 	exit 1
 }
-echo "以上$(jq -rS ".\"${DEVICE}\""' | length' "${TMPFILE}")個のSHSH取得が可能です。"
+echo "以上$(jq -rS ".\"${DEVICE}\""' | length' <(printf "%s" "${JSON}"))個のSHSH取得が可能です。"
